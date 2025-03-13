49879c971b4e7b391c213b34d11149521d94501b
khalil <khalil.boudali41@gmail.com>
2025-03-13 02:06:23 +0100

    fusion finale

--- projet.py
+++ projet.py
@@ -1,7 +1,14 @@
-
 def somme (T):
   s=0
   for t in T:
       s+=t
       return s
+
+data[1,3,5]
+if data :
+    print('la somme est : ',sum(data))
+    print('le min est :',min(data))
+    print('le max est :',max(data))
+else :
+    print('dossier vide ')
 
